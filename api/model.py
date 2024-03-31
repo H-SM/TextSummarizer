9ee1b5e7932caad7cfa756c7a16e2a1888a2e06d
your.user.name <harmanmalht777@gmail.com>
2024-03-31 18:08:50 +0530

    feat: working backend for the application

--- api/model.py
+++ api/model.py
@@ -29,39 +29,6 @@ nltk.download('averaged_perceptron_tagger')
 Stopwords = set(stopwords.words('english'))
 wordlemmatizer = WordNetLemmatizer()
 
-# %% [markdown]
-# ##### 2. making web-scrapper
-
-# %%
-# URL = "https://hackernoon.com/simplifying-the-crazy-world-of-linux-distros"
-# URL = "https://medium.com/@palashm0002/understanding-the-basics-of-natural-language-processing-nlp-52b67cf954cb"
-URL = "https://hackernoon.com/nvidia-throws-gamers-under-the-bus"
-# URL = "https://hackernoon.com/the-eggs-destined-to-give-birth-to-queens"
-
-# %%
-r = requests.get(URL)
-
-# %%
-soup = BeautifulSoup(r.text, 'html.parser')
-results = soup.find_all(['h1', 'p'])
-text = [result.text for result in results]
-ARTICLE = ' '.join(text)
-script_tag = soup.find('script', type='application/ld+json')
-
-if script_tag:
-    # Parse JSON data
-    json_data = json.loads(script_tag.string)
-    
-    # Extract articleBody
-    article_body = json_data.get('articleBody', '')
-
-    ARTICLE = ARTICLE +". " + article_body
-else:
-    print("JSON-LD script tag not found.")
-
-
-# %% [markdown]
-# #### 3. Text propressing - and implementing TFIDF 
 
 # %%
 def lemmatize_words(words):
@@ -78,25 +45,10 @@ def stem_words(words):
     return stemmed_words
 
 
-# %%
 def remove_special_characters(text):
     regex = r'[^a-zA-Z0-9\s]'
     text = re.sub(regex,'',text)
     return text
-
-# %%
-tokenized_sentence = sent_tokenize(ARTICLE)
-text = remove_special_characters(str(ARTICLE))
-text = re.sub(r'\d+', '', text)
-
-
-
-# %%
-tokenized_words_with_stopwords = word_tokenize(text)
-tokenized_words = [word for word in tokenized_words_with_stopwords if word not in Stopwords]
-tokenized_words = [word for word in tokenized_words if len(word) > 1]
-tokenized_words = [word.lower() for word in tokenized_words]
-tokenized_words = lemmatize_words(tokenized_words)
 
 # %%
 def freq(words):
@@ -110,15 +62,6 @@ def freq(words):
        dict_freq[word] = words.count(word)
     return dict_freq
 
-
-
-# %%
-word_freq = freq(tokenized_words)
-input_user = int(input('Percentage of information to retain(in percent):'))
-no_of_sentences = int((input_user * len(tokenized_sentence))/100)
-
-
-print(no_of_sentences)
 
 # %%
 def pos_tagging(text):
@@ -185,43 +128,78 @@ def sentence_importance(sentence,dict_freq,sentences):
 # ##### 6. Summarize Text
 
 # %%
-c = 1
-sentence_with_importance = {}
-for sent in tokenized_sentence:
-    sentenceimp = sentence_importance(sent,word_freq,tokenized_sentence)    
-    sentence_with_importance[c] = sentenceimp    
-    c = c+1
-sentence_with_importance = sorted(sentence_with_importance.items(), key=operator.itemgetter(1),reverse=True)
 
-cnt = 0
-summary = []
-sentence_no = []
-for word_prob in sentence_with_importance:
-    if cnt < no_of_sentences:
-        sentence_no.append(word_prob[0])
-        cnt = cnt+1
+
+# %%
+# with open('summary.txt', 'w') as f:
+#     f.write(summary)
+
+
+def generate_summary(url, percentage):
+    # URL = "https://hackernoon.com/nvidia-throws-gamers-under-the-bus"
+
+    r = requests.get(url)
+ 
+    soup = BeautifulSoup(r.text, 'html.parser')
+    results = soup.find_all(['h1', 'p'])
+    text = [result.text for result in results]
+    ARTICLE = ' '.join(text)
+    script_tag = soup.find('script', type='application/ld+json')
+
+    if script_tag:
+        # Parse JSON data
+        json_data = json.loads(script_tag.string)
+
+        # Extract articleBody
+        article_body = json_data.get('articleBody', '')
+
+        ARTICLE = ARTICLE +". " + article_body
     else:
-      break
-sentence_no.sort()
-cnt = 1
-for sentence in tokenized_sentence:
-    if cnt in sentence_no:
-       summary.append(sentence)
-    cnt = cnt+1
-
-summary = " ".join(summary)
-# summary = summary.replace('&apos;s', '')
-summary = summary.replace('&apos;', "'")
-summary = summary.replace('&quot;', '"')
+        return "-- CONTENT RESTRICTED --",0,0
 
 
-summary
-
-# %%
-print(len(ARTICLE), len(summary))
-
-# %%
-with open('summary.txt', 'w') as f:
-    f.write(summary)
+    tokenized_sentence = sent_tokenize(ARTICLE)
+    text = remove_special_characters(str(ARTICLE))
+    text = re.sub(r'\d+', '', text)
 
 
+    tokenized_words_with_stopwords = word_tokenize(text)
+    tokenized_words = [word for word in tokenized_words_with_stopwords if word not in Stopwords]
+    tokenized_words = [word for word in tokenized_words if len(word) > 1]
+    tokenized_words = [word.lower() for word in tokenized_words]
+    tokenized_words = lemmatize_words(tokenized_words)
+
+    word_freq = freq(tokenized_words)
+    input_user = percentage
+    no_of_sentences = int((input_user * len(tokenized_sentence))/100)
+
+    c = 1
+    sentence_with_importance = {}
+    for sent in tokenized_sentence:
+        sentenceimp = sentence_importance(sent,word_freq,tokenized_sentence)    
+        sentence_with_importance[c] = sentenceimp    
+        c = c+1
+    sentence_with_importance = sorted(sentence_with_importance.items(),     key=operator.itemgetter(1),reverse=True)
+
+    cnt = 0
+    summary = []
+    sentence_no = []
+    for word_prob in sentence_with_importance:
+        if cnt < no_of_sentences:
+            sentence_no.append(word_prob[0])
+            cnt = cnt+1
+        else:
+          break
+    sentence_no.sort()
+    cnt = 1
+    for sentence in tokenized_sentence:
+        if cnt in sentence_no:
+           summary.append(sentence)
+        cnt = cnt+1
+
+    summary = " ".join(summary)
+    # summary = summary.replace('&apos;s', '')
+    summary = summary.replace('&apos;', "'")
+    summary = summary.replace('&quot;', '"')
+
+    return summary, len(ARTICLE), len(summary)
--- api/server.py
+++ api/server.py
@@ -1,6 +1,7 @@
 import time
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from model import generate_summary
 
 app = Flask(__name__)
 CORS(app)
@@ -18,5 +19,26 @@ def get_members():
 @app.route('/')
 def home_page():
     return {"page": "this is server"}
+
+@app.route('/summary', methods=['POST'])
+def your_route_function():
+    if not request.json:
+        return jsonify({"error": "Request body must be in JSON format"}), 400
+
+    url = request.json.get('url')
+    percentage_str = request.json.get('percentage')
+
+    if percentage_str is None:
+        return jsonify({"error": "Percentage not provided"}), 400
+
+    try:
+        percentage = int(percentage_str)
+    except ValueError:
+        return jsonify({"error": "Percentage must be an integer"}), 400
+
+    summary, lenarticle, lensummary = generate_summary(url, percentage)
+
+    # Return the summary
+    return jsonify({"summary": summary, "lengthA" : lenarticle, "lengthS" : lensummary})
 
 #  flask --app server run --debug 
