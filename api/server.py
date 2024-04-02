5be9c5434e052f412723da5eeea9a47a2dc83bc0
H-SM <harmanmalht777@gmail.com>
2024-04-02 22:35:39 +0530

    feat: context summarization addition

--- api/server.py
+++ api/server.py
@@ -1,7 +1,7 @@
 import time
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from model import generate_summary
+from model import generate_summary, generate_summary_context
 
 app = Flask(__name__)
 CORS(app)
@@ -21,7 +21,7 @@ def home():
     return "Text Summarizer @ HSM - Backend", 200
 
 @app.route('/summary', methods=['POST'])
-def your_route_function():
+def summary_here():
     if not request.json:
         return jsonify({"error": "Request body must be in JSON format"}), 400
 
@@ -41,4 +41,25 @@ def your_route_function():
     # Return the summary
     return jsonify({"summary": summary, "lengthA" : lenarticle, "lengthS" : lensummary})
 
-#  flask --app index run --debug 
+@app.route('/summarycontext', methods=['POST'])
+def context_summary_here():
+    if not request.json:
+        return jsonify({"error": "Request body must be in JSON format"}), 400
+
+    context = request.json.get('context')
+    percentage_str = request.json.get('percentage')
+
+    if percentage_str is None:
+        return jsonify({"error": "Percentage not provided"}), 400
+
+    try:
+        percentage = int(percentage_str)
+    except ValueError:
+        return jsonify({"error": "Percentage must be an integer"}), 400
+
+    summary, lenarticle, lensummary = generate_summary_context(context, percentage)
+
+    # Return the summary
+    return jsonify({"summary": summary, "lengthA" : lenarticle, "lengthS" : lensummary})
+
+#  flask --app server run --debug 
--- application/src/App.js
+++ application/src/App.js
@@ -6,21 +6,34 @@ function App() {
   const [articleLen, setArticleLen] = useState(0);
   const [summLen, setSummLen] = useState(0);
   const [showBox, setShowBox] = useState(0);
+  const [showContext, setShowContext] = useState(1);
   const [percentage, setPercentage] = useState(0);
   const [url, setUrl] = useState("");
+  const [context, setContext] = useState("");
 
   const handleUrlChange = (event) => {
     const newValue = event.target.value;
     setUrl(newValue);
   };
+
+  const handleContextChange = (event) => {
+    const newValue = event.target.value;
+    setContext(newValue);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const endpointUrl = "http://localhost:5000/summary";
+    const endpointUrl = showContext ? "http://localhost:5000/summarycontext" : "http://localhost:5000/summary";
+    console.log(endpointUrl);
     setShowBox(1);
     setSummary("Loading...");
     setArticleLen(0);
     setSummLen(0);
-    fetchSummary(endpointUrl);
+    {
+      showContext == 1
+        ? fetchSummaryContext(endpointUrl)
+        : fetchSummary(endpointUrl);
+    }
   };
   const fetchSummary = async (endpointUrl) => {
     try {
@@ -31,6 +44,26 @@ function App() {
         },
         body: JSON.stringify({
           url: url,
+          percentage: percentage,
+        }),
+      });
+      const data = await response.json();
+      setSummary(data.summary);
+      setArticleLen(data.lengthA);
+      setSummLen(data.lengthS);
+    } catch (error) {
+      console.error("Error fetching summary:", error);
+    }
+  };
+  const fetchSummaryContext = async (endpointUrl) => {
+    try {
+      const response = await fetch(endpointUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          context: context,
           percentage: percentage,
         }),
       });
@@ -56,26 +89,46 @@ function App() {
     <div className="text-[2rem] mx-[5rem] mt-[10rem] font-Nothing font-medium h-full">
       <div className="w-fit h-fit flex gap-2">
         TEXT SUMMARIZER @ HSM
-        <img src={logo} alt="logo" className="w-[3rem] h-[3rem]"/>
+        <img src={logo} alt="logo" className="w-[3rem] h-[3rem]" />
       </div>
       <form onSubmit={handleSubmit} class="mt-8 font-SourceCodePro">
         <div class="relative z-0 w-full mb-5 group">
-          <input
-            type="text"
-            name="floating_URL"
-            id="floating_URL"
-            class="block py-2.5 px-0 w-[49vw] text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer"
-            placeholder=" "
-            required
-            onChange={handleUrlChange}
-            pattern="https?://.+"
-          />
-          <label
-            for="floating_URL"
-            class="peer-focus:font-medium absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            URL
-          </label>
+          {showContext == 1 ? (
+            <textarea
+              type="text"
+              name="floating_URL"
+              id="floating_URL"
+              required
+              onChange={handleContextChange}
+              class="lock py-2.5 px-0 w-[80vw] text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer h-[50vh]"
+            ></textarea>
+          ) : (
+            <input
+              type="text"
+              name="floating_URL"
+              id="floating_URL"
+              class="block py-2.5 px-0 w-[49vw] text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 peer"
+              placeholder=" "
+              required
+              onChange={handleUrlChange}
+              pattern="https?://.+"
+            />
+          )}
+          {showContext == 0 ? (
+            <label
+              for="floating_URL"
+              class="peer-focus:font-medium absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              URL
+            </label>
+          ) : (
+            <label
+              for="floating_URL"
+              class="font-medium absolute text-[1.2rem] duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10"
+            >
+              Context
+            </label>
+          )}
         </div>
         <div class="relative z-0 w-[4em] mb-5 group">
           <input
@@ -96,12 +149,26 @@ function App() {
             Percentage
           </label>
         </div>
-        <button
-          type="submit"
-          className="border border-1 border-black border-opacity-55 font-Nothing font-semibold px-5 py-3 bg-transparentw-[8rem] h-[3rem] rounded-lg text-[1rem] flex justify-center items-center scale-95 hover:scale-100 transition ease-in-out duration-200"
-        >
-          SUBMIT
-        </button>
+        <div className="flex gap-2">
+          <button
+            type="submit"
+            className="border border-1 border-black border-opacity-55 font-Nothing font-semibold px-5 py-3 bg-transparentw-[8rem] h-[3rem] rounded-lg text-[1rem] flex justify-center items-center scale-95 hover:scale-100 transition ease-in-out duration-200"
+          >
+            SUBMIT
+          </button>
+          <button
+            className="border border-1 border-gray-700 text-gray-700 border-opacity-55 font-Nothing font-semibold px-5 py-3 bg-transparentw-[8rem] h-[3rem] rounded-lg text-[1rem] flex justify-center items-center scale-95 hover:scale-100 transition ease-in-out duration-200" onClick={(e) => {
+              e.preventDefault();
+              setShowContext(!showContext);
+              setSummary("");
+              setArticleLen(0);
+              setSummLen(0);
+              setShowBox(0);
+            }}
+          >
+            {showContext == 1 ? "USE URL" : "USE CONTEXT"}
+          </button>
+        </div>
       </form>
       {showBox === 1 && (
         <div>
